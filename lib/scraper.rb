214c2a9ce8728fe1022f14c59581269980a24fb9
Eric <ericontiveros123@gmail.com>
2021-07-27 19:26:57 -0700

    commit tree 12a1510523528bf851e47978c9b49647632dfaac

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,4 +1,25 @@
 require 'nokogiri'
 require 'open-uri'
+require 'pry'
 
-html = open("https://flatironschool.com/")
+require_relative './course.rb'
+
+class Scraper
+
+  def get_page
+
+    doc = Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
+
+    binding.pry
+  end
+
+    doc.css(".post").each do |post|
+      course = Course.new
+      course.title = post.css("h2").text
+      course.schedule = post.css(".date").text
+      course.description = post.css("p").text
+    
+    end
+  end
+
+  Scraper.new.get_page
